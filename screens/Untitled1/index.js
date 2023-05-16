9a220e855bb1a8db9e4d2cd4f0bbaec8b17b5873
Ravinder sales <ravinder.y@crowdbotics.com>
2023-05-16 17:41:03 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,53 @@
+import { Text } from "react-native";
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
-    }}></ScrollView>
+    }}>
+        <Pressable>
+          <View style={styles.peNcplka}>
+            <Text style={styles.mZLLVeNO}>Lorem ipsum…</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <Text style={styles.QhAOMaer}>Lorem ipsum…</Text>
+        </Pressable>
+      </ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
+  },
+  peNcplka: {
+    height: 88,
+    width: 181,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  QhAOMaer: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  mZLLVeNO: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled1;
