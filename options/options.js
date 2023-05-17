55968f45d60368ecf49dbca1482ef1378b3be22b
Ravinder sales <ravinder.y@crowdbotics.com>
2023-05-17 20:24:16 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -6,7 +6,6 @@ export const globalOptions = {
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    "title": "App Menu TT",
-    "copy": "Routes available! TT"
+    "copy": "Routes available!"
   }
 };
